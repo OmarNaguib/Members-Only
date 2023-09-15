8f73c306c284b6ac834540068ff928dc9eb19dba
Omar <55961349+OmarNaguib@users.noreply.github.com>
2023-09-15 17:35:49 +0300

    Setup ejs layout

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
 const mongoose = require("mongoose");
+const expressLayouts = require("express-ejs-layouts");
 require("dotenv").config();
 
 // Set up mongoose connection
@@ -25,6 +26,9 @@ const app = express();
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
+app.use(expressLayouts);
+app.set("layout", "./layout.ejs");
+
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -44,6 +48,7 @@ app.use((err, req, res, next) => {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get("env") === "development" ? err : {};
+  res.locals.title = "Error";
 
   // render the error page
   res.status(err.status || 500);
