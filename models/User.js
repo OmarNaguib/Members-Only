031a8f23f62abea419c2d76d0a4e8d34d3fb4767
Omar <55961349+OmarNaguib@users.noreply.github.com>
2023-09-15 17:42:45 +0300

    Create user model

---
+++ models/User.js
@@ -0,0 +1,16 @@
+const { model, Schema } = require("mongoose");
+
+const userSchema = new Schema({
+  username: { type: String, required: true },
+  password: { type: String, required: true },
+  firstName: { type: String, required: true },
+  lastName: { type: String, required: true },
+});
+
+userSchema.virtual("fullName").get(function () {
+  return `${this.firstName} ${this.lastName}`;
+});
+
+const User = model("User");
+
+module.exports = User;
