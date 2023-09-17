ba5c99d4652be5f7822b6191abb20b2ad4e6a68b
Omar <55961349+OmarNaguib@users.noreply.github.com>
2023-09-17 14:51:23 +0300

    Format time

--- models/Post.js
+++ models/Post.js
@@ -1,9 +1,15 @@
+/* eslint-disable  */
 const { model, Schema } = require("mongoose");
+const { DateTime } = require("luxon");
 
 const postSchema = new Schema({
   postText: { type: String, required: true },
   author: { type: Schema.Types.ObjectId, required: true },
   date: { type: Date, required: true },
+});
+
+postSchema.virtual("formattedDate").get(function () {
+  return DateTime.fromJSDate(this.date).toLocaleString(DateTime.DATE_MED);
 });
 
 const Post = model("Post", postSchema);
